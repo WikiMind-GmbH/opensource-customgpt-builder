a826e9808d59996c61e3ad145c8e5cee5e5c4b97
khushi <khushikala18@gmail.com>
2025-07-10 17:20:51 +0200

    changed tags

--- backend/app.py
+++ backend/app.py
@@ -46,7 +46,7 @@ app.add_middleware(
 # Retrives the history of a chat by ID
 @app.post(
     "/chat-history-by-id",
-    tags=["chatService"],
+    tags=["Chat"],
     summary="Fetches the history of a particular chat id. Identifier = 5",
     response_description="retrives the list of messages and Model ID (0: ChatGPT40, -1: Error, 1/custom_gpt_id: CustomGPTName)",
     response_model=ChatHistory,
@@ -60,7 +60,7 @@ async def get_chat_history(chat_id: int) -> ChatHistory:
 # Retrieves the list of Chat Summaries
 @app.get(
     "/get-chat-summaries",
-    tags=["chatService"],
+    tags=["Chat"],
     summary="One Liner summary of all chats in the Chat List.  Identifier = 3",
     response_description="List of chats summary in one line with a Chat ID",
     response_model=list[ChatSummary],
@@ -74,7 +74,7 @@ async def get_chat_summaries() -> list[ChatSummary]:
 # Send the User Message to the GPT Model
 @app.post(
     "/send-user-message",
-    tags=["chatService"],
+    tags=["Chat"],
     summary="Process the user chat message and send response back from the Model selected, Indentifier = 8",
     response_description="Generates the response from the assistant with a conversation ID",
     response_model=AssistantMessage,
@@ -88,7 +88,7 @@ async def send_user_message(request: UserMessageRequest) -> AssistantMessage:
 # Retrieve All Custom GPTs (List)
 @app.get(
     "/retreive-all-custom-gpts",
-    tags=["customGPTService"],
+    tags=["customGPTs"],
     summary="Dispalys all the custom GPTs created, Indentfier = 4",
     response_description="Returns a list of custom gpts created",
     response_model=list[ExistingCustomGPT],
@@ -102,7 +102,7 @@ async def retreive_all_custom_gpts() -> list[ExistingCustomGPT]:
 # Retrieve a custom gpt by ID
 @app.get(
     "/get-custom-gpt-infos",
-    tags=["customGPTService"],
+    tags=["customGPTs"],
     summary="User chats with Custom GPT, indetifier = 13",
     response_description="Information of Custom GPT so as to the Navigate to its Chat Page",
     response_model=ExistingCustomGPT,
@@ -116,7 +116,7 @@ async def get_custom_gpt_by_id(custom_gpt_id: int):
 # Deletes the Custom GPT By ID
 @app.delete(
     "/delete-custom-gpt",
-    tags=["customGPTService"],
+    tags=["customGPTs"],
     summary="delete the existing custom GPT by ID, identifier = 15",
     response_description="Send the status of deleted successfully(true) or failure(false)",
     operation_id="deleteCustomGPT",
@@ -129,7 +129,7 @@ async def delete_custom_gpt(custom_gpt_id: int) -> DeleteCustomGPTStatus:
 # Create a New Custom GPT
 @app.post(
     "/create-or-edit-custom-gpt",
-    tags=["customGPTService"],
+    tags=["customGPTs"],
     summary="Takes the input from user to create a custom gpt (Name, instruction), Indentfier = 11/ create",
     response_description="Responds the status of custom gpt created successfully(true) or failed(false) with custom_gpt_id",
     response_model=CreateOrEditCustomGPTStatus,
