704a789a0d0533508a3ec1182df6afa86e30ec09
AlsanArcade <alsandritter@web.de>
2026-07-14 15:52:32 +0200

    upldated client - new ts generator

--- frontend/src/client/models/ChatSummary.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type ChatSummary = {
-    chat_id: string;
-    chat_summary: string;
-};
-
--- frontend/src/client/models/RoleQuery.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export enum RoleQuery {
-    USER = 'user',
-    ASSISTANT = 'assistant',
-}
